48cd468452b9f1631f2fb7890fde4fa53c510708
CollinNguyen <114682116+ThanhBinh1906@users.noreply.github.com>
2026-05-21 08:36:49 +0700

    User Dashboard Update
    Xóa bớt file thừa index.css và index.jsx trong folder page

--- src/page/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-export default function Main() {
-  return (
-    <div className="main-container w-[613px] h-[888px] bg-[#fff] relative overflow-hidden mx-auto my-0" />
-  );
-}
